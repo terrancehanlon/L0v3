fdd1806e8ebad1a7833dee2c025cce0171e32137
Terrance <terrance.hanlon@ndsu.edu>
2020-02-08 17:20:15 -0600

    failed physics

--- main.lua
+++ main.lua
@@ -33,17 +33,36 @@ maxPillarSize = 300
 --this variable extends outside the screen by this number of pixels, just to give it a more immersive feeling
 pillarPadding = 15
 
+enemyPaddleX = screenX/7
+enemyPaddleY = 75
+
+enemyPaddleSizeX = 75
+enemyPaddleSizeY = 15
+
+enemyPaddleSpeed = 15
+
+
 paddleX = screenX / 2
 paddleY = screenY - 75
 
-paddleSizeX = 75
+paddle_velocity = 1.50
+
+paddleSizeX = 150
 paddleSizeY = 15
 
-paddleSpeedX = 350
+paddleSpeedX = 850
+
+relativeIntersectX = 1
+normalizedRelativeIntersect = 1
+bounceAngle = 1
+MAXBALLANGLE = 75
+velocX = 500
+velocY = 500
 function love.load()
   
   --world = love.physics.newWorld(0, 200, true)  
   love.window.setFullscreen(true)
+  love.graphics.setColor(255, 0 , 0, 50)
   love.graphics.setBackgroundColor(0, 255, 255, 50)
   
   screenX = love.graphics.getWidth()
@@ -65,19 +84,50 @@ end
 function collision()
      -- print('ball x: ' .. ballX .. ' ball y: ' .. ballY)
       --print('paddle x: ' .. paddleX .. ' paddle y: ' .. paddleY)
-    if ballX <= (paddleX + paddleSizeX) and ballX >= paddleX then
-      if ballY <= (paddleY + paddleSizeY) and ballY >= paddleY then
-          return true
-      end
-      return false
+      if ballX <= (paddleX + paddleSizeX) and ballX >= paddleX then
+        if ballY <= (paddleY + paddleSizeY) and ballY >= paddleY then
+            relativeIntersectX = (paddleSizeX + (paddleSizeX/2)) - ballX -- gets the distance the ball is from the middle
+            normalizedRelativeIntersect = (relativeIntersectX/(paddleSizeX/2))
+            bounceAngle = normalizedRelativeIntersect * MAXBALLANGLE
+            print(bounceAngle)
+            velocX = ballX_speed * math.cos(bounceAngle)
+            -- velocY = ballY_speed * math.sin(bounceAngle)
+            return true
+        end
+        return false
+    end
+    return false
+end
+
+function enemyCollision()
+  if ballX <= (enemyPaddleX + enemyPaddleSizeX) and ballX >= enemyPaddleX then
+    if ballY <= (enemyPaddleY + enemyPaddleSizeY) and ballY >= enemyPaddleY then
+        return true
+    end
+    return false
+end
+return false
+end
+
+function enemyMove()
+
+  if enemyPaddleX < ballX then
+    -- print('going to right')
+    enemyPaddleX = enemyPaddleX + enemyPaddleSpeed
+  elseif enemyPaddleX > ballX then
+    -- print('going to left')
+    enemyPaddleX = enemyPaddleX - enemyPaddleSpeed
+  else
+    print('do nothing')
   end
-  return false
+
 end
 
 function love.update(dt)
   --world:update(dt)
   -- if love.keypressed
   -- if love.keyboard.isDown("space") then
+  enemyMove()
     
     if love.keyboard.isDown("a") == true then
       paddleX = paddleX - paddleSpeedX * dt
@@ -98,11 +148,11 @@ function love.update(dt)
     end
     
     if ballY >= screenY then
-      goingUp = true
       goingDown = false
+      goingUp = true
     end
-    
-    
+     
+        
     if ballX <= 0 then
       goingLeft = false
       goingRight = true
@@ -110,23 +160,31 @@ function love.update(dt)
 
     
     if collision() == true then
-      print('ball x: ' .. ballX .. ' ball y: ' .. ballY)
-      print('paddle x: ' .. paddleX .. ' paddle y: ' .. paddleY)
       goingUp = true
       goingDown = false
     end
-    
-    if goingLeft then
-      ballX = ballX - 300 * dt
-    elseif goingRight then
-      ballX = ballX + ballX_speed * dt
-    end
+
+    -- if enemyCollision() == true then
+    --   goingUp = false;
+    --   goingDown = true;
+    -- end
     
     if goingDown then
-      ballY = ballY + ballY_speed * dt
+      ballY = (ballY + (velocY * dt))
     elseif goingUp then
-      ballY = ballY - ballY_speed * dt
+      ballY = (ballY - (velocY * dt))
     end
+    if goingLeft then
+      ballX = (ballX - (dt * velocX))
+    elseif goingRight then
+      ballX = (ballX + (dt * velocY))
+    end
+
+    if ballX >= screenX then
+      goingLeft = true
+      goingRight = false
+    end
+    
     
   
   
@@ -145,6 +203,7 @@ function love.draw()
     love.graphics.circle("fill", ballX, ballY, 50, 25)
     --love.graphics.rectangle('fill', 150, (screenY - 100), 25, 105)
     love.graphics.rectangle('fill', paddleX, paddleY, paddleSizeX, paddleSizeY)
+    love.graphics.rectangle('fill', enemyPaddleX, enemyPaddleY, enemyPaddleSizeX, enemyPaddleSizeY)
     
     --local i = 0;
     
